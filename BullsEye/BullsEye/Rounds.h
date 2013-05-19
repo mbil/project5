a02cf5f0f69c615a960f907bd1ba5d5f20abff62
Myrthe Bil <myrthebil@gmail.com>
2013-05-19 20:41:46 +0200

    gitignore + rounds

---
+++ BullsEye/BullsEye/Rounds.h
@@ -0,0 +1,13 @@
+//
+//  Rounds.h
+//  BullsEye
+//
+//  Created by Murph on 5/14/13.
+//  Copyright (c) 2013 Hollance. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Rounds : NSObject
+
+@end
---
+++ BullsEye/BullsEye/Rounds.m
@@ -0,0 +1,13 @@
+//
+//  Rounds.m
+//  BullsEye
+//
+//  Created by Murph on 5/14/13.
+//  Copyright (c) 2013 Hollance. All rights reserved.
+//
+
+#import "Rounds.h"
+
+@implementation Rounds
+
+@end
